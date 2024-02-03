be498c83b66343aa11f124e78690d41cb5aaaf4d
Nadaadizer9611 <67803943+Nadaadizer9611@users.noreply.github.com>
2024-02-03 02:54:51 -0300

    Update Items.java

--- src/example/Items.java
+++ src/example/Items.java
@@ -1,1 +1,19 @@
+package beyond.content;
 
+import arc.graphics.*;
+import arc.struct.*;
+import mindustry.type.*;
+
+public class Items{
+    public static Item
+    mangle;
+    public static final Seq<Item> serpuloItems = new Seq<>();
+
+    public static void load(){
+        mangle = new Item("Mangle", Color.valueOf("d99d73")){{
+            hardness = 1;
+            cost = 0.5f;
+            alwaysUnlocked = true;
+        }};
+    }
+}
